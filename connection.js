46bde96e7b86e3f02561a211cf32e458f36b1e3e
Emma Bishop <emma.b.bishop@gmail.com>
2016-01-12 21:11:02 -0500

    Created connections class

--- connection.js
+++ connection.js
@@ -1,5 +1,12 @@
 var Connection = function(nextNode, condition) {
-
+	this.nextNode = nextNode;
+	this.condition = condition;
 };
 
+Connection.prototype.test = function(condition) {
+	if(this.condition == null || this.condition == condition){
+		return true;
+	}
+	return false;
+};
 module.exports = Connection;
