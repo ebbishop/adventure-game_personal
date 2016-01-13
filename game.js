6a230a9cc45f6bebcd947b46002d175865ccd20b
Emma Bishop <emma.b.bishop@gmail.com>
2016-01-12 22:41:42 -0500

    Added Game class functionality. Includes .addNode, .connect

--- game.js
+++ game.js
@@ -1,7 +1,49 @@
 var Node = require('./node');
 
 var Game = function() {
+	this.nodes = {};
+	this.startingPoint = null;
+};
+
+// Object structure, for reference:
+
+// nodes {
+// 	myTitle: {title: myTitle, text: myText},
+// 	nextTitle: {title: nextTitle, text: nextText}
+// }
+
+// Node {
+// 	this.title = title;
+// 	this.text = text;
+// 	this.connections = [];
+// }
+
+Game.prototype.addNode = function(title, text) {
+	if(this.nodes[title]){
+		// console.log('This game already has a node called', title);
+		throw new Error('This game already has a node called', title);
+	}
+
+	var node = new Node(title, text);
+
+	if(!this.startingPoint){
+		this.startingPoint = node;
+	}
+	this.nodes[title] = node;
+	return node;
 
 };
 
+Game.prototype.getNode = function(title) {
+	return this.nodes[title];
+};
+
+Game.prototype.connect = function(currentNodeTitle, nextNodeTitle, condition) {
+	if (!this.nodes[currentNodeTitle]){
+		throw new Error('Node', currentNodeTitle, 'does not exist')
+	};
+	var currentNode = this.nodes[currentNodeTitle];
+	var nextNode = this.nodes[nextNodeTitle];
+	currentNode.connect(nextNode, condition);
+};
 module.exports = Game;
